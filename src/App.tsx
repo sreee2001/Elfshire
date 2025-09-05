e27fee90f61305855bdd281276ae4accc162435a
Srikanth Tangella <srikanth.tangella@gmail.com>
2025-09-04 20:16:27 -0500

    Button with error checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Alert from "./Alert";
+import Button from "./components/Button";
 
 function TownCenter() {
   return (
@@ -6,6 +7,12 @@ function TownCenter() {
       <Alert>
         Welcome to the <span className="font-weight-bold">Town Center!</span>
       </Alert>
+      <Button onClick={() => console.log("Clicked!")}>
+        <span className="font-weight-bold">Click Me!</span>
+      </Button>
+      <Button color="secondary" onClick={() => console.log("Clicked!")}>
+        <span className="font-weight-bold">Click Me!</span>
+      </Button>
     </div>
   );
 }
