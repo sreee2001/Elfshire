1be773ef04e7f1065635ec74641065df174a4ba6
Srikanth Tangella <srikanth.tangella@gmail.com>
2025-09-09 17:24:18 -0500

    refactor: restructure HomePage layout and enhance navigation links

--- elfshire_ui/src/data/demo/Demo.tsx
+++ elfshire_ui/src/data/demo/Demo.tsx
@@ -12,28 +12,51 @@ export default function HomePage() {
   ]);
 
   return (
-    <div className={commonStyles.container}>
-      <div className={commonStyles.content}>
-        <h1 className={commonStyles.heading}>
+    // <div className={commonStyles.container}>
+    <div style={{ display: "flex", minHeight: "100vh", flexDirection: "row" }}>
+      {/* Navigation Window */}
+      <nav className={commonStyles.contentNavigation}>
+        <h1 className={commonStyles.headingNavigation}>
           Welcome to TypeScript Demo Page
         </h1>
+        <ul style={{ listStyle: "none", padding: 0 }}>
+          <li>
+            <a href="#/demo#input-data">Input Data</a>
+          </li>
+          <li>
+            <a href="#/demo#charts">Charts</a>
+          </li>
+          <li>
+            <a href="#/demo">Demo</a>
+          </li>
+          {/* Add more links as needed */}
+        </ul>
+      </nav>
+      {/* Main Content Section */}
+      <div className={commonStyles.content} style={{ flex: 1 }}>
         <p className={commonStyles.intro}>
           I will be using this page to show various features of TypeScript and
           React.
         </p>
         <div className={commonStyles.sections}>
-          <div className={commonStyles.section}>
-            <div className={commonStyles.sectionTitle}>Charts</div>
+          {/* Input Section */}
+          <div className={commonStyles.section} id="input-data">
+            <div className={commonStyles.sectionTitle}>Input Data</div>
             {
               <div className={commonStyles.sectionText}>
                 <InputDataTable rows={inputData} setRows={setInputData} />
-                <div style={{ height: "300px", width: "300px" }}>
-                  <ErrorBoundary>
-                    <ChartsDemo data={inputData} />
-                  </ErrorBoundary>
-                </div>
               </div>
             }
+          </div>
+          <div className={commonStyles.section} id="charts">
+            {/* Display Charts here */}
+            <div className={commonStyles.sectionText}>
+              <div>
+                <ErrorBoundary>
+                  <ChartsDemo data={inputData} />
+                </ErrorBoundary>
+              </div>
+            </div>
           </div>
         </div>
       </div>
